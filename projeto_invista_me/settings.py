6a005d04eb4f49c360940d003257749210125bd9
guiomenezes <guilherme.menezes@outlook.com>
2024-08-03 19:04:39 -0300

    commit base de dados

--- projeto_invista_me/settings.py
+++ projeto_invista_me/settings.py
@@ -79,8 +79,12 @@ WSGI_APPLICATION = 'projeto_invista_me.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'postgres', 
+        'USER': 'postgres.vdjhkellhdjeancaemnw', 
+        'PASSWORD': '4dhlKFFmbKEQ9BNE',
+        'HOST': 'aws-0-sa-east-1.pooler.supabase.com', 
+        'PORT': '6543'
     }
 }
 
